15a5c464e1a104f801f3b4cc2137bf6670284a2e
Pezza242 <periyearwood@gmail.com>
2026-05-05 19:41:51 +0100

    Added function to the select options
    Now, when a city is selected in the select dropdown option, it will change the name, time and date to the exact city

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-let date = document.querySelector("#city-date");
-let tzDate = moment.tz("Europe/Paris").format("Do MMMM YYYY");
+function updateParisTime() {
+  let date = document.querySelector("#city-date");
+  let tzDate = moment.tz("Europe/Paris").format("Do MMMM YYYY");
 
-date.innerHTML = tzDate;
+  date.innerHTML = tzDate;
 
-function updateTime() {
   let time = document.querySelector("#city-time");
   let tzTime = moment.tz("Europe/Paris").format("H:mm:ss [<small>]A[</small>]");
 
   time.innerHTML = tzTime;
 }
-updateTime();
-setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTz = event.target.value;
+  let cityName = cityTz.replace("_", " ").split("/")[1];
+  let cityDate = moment.tz(cityTz).format("Do MMMM YYYY");
+  let cityTime = moment.tz(cityTz).format("H:mm:ss [<small>]A[</small>]");
+  let city = document.querySelector("#city-section");
+
+  city.innerHTML = `       
+   <div class="left">        
+            <h2 id="city-name">${cityName}</h2>
+          <p id="city-date">${cityDate}</p>
+        </div>
+          <div id="city-time">${cityTime}</div>`;
+}
+
+updateParisTime();
+setInterval(updateParisTime, 1000);
+let selectCity = document.querySelector("#select");
+selectCity.addEventListener("change", updateCity);
