607e32e44b1d7b6106edf5ad2480c1a514561956
Pezza242 <periyearwood@gmail.com>
2026-05-05 21:50:07 +0100

    Fixed the select option issue
    Now, instead of it only just showing for a second, then going back to all the cities, when you click on the select option, it shows the single city and there's a back button that allows you to go back to all the cities

--- src/index.js
+++ src/index.js
@@ -27,28 +27,53 @@ function updateCityTime() {
   citySection.innerHTML = addCities;
 }
 
-function updateCity(event) {
-  let cityTz = event.target.value;
-  if (cityTz === "current") {
-    cityTz = moment.tz.guess(true);
-  }
-  let cityName = cityTz.replace("_", " ").split("/")[1];
-  let cityDate = moment.tz(cityTz).format("Do MMMM YYYY");
-  let cityTime = moment.tz(cityTz).format("H:mm:ss [<small>]A[</small>]");
-  let city = document.querySelector("#city-section");
+function showSingleCity(cityTz) {
+  function update() {
+    let cityName = cityTz.replace("_", " ").split("/")[1];
+    let cityDate = moment.tz(cityTz).format("Do MMMM YYYY");
+    let cityTime = moment.tz(cityTz).format("H:mm:ss [<small>]A[</small>]");
 
-  city.innerHTML = `  
-  <div class="city">
-      <div class="left">        
+    let city = document.querySelector("#city-section");
+
+    city.innerHTML = `
+      <div class="city">
+        <div class="left">        
           <h2 class="city-name">${cityName}</h2>
           <p class="city-date">${cityDate}</p>
         </div>
-          <div class="city-time">${cityTime}</div>
-  </div>`;
+        <div class="city-time">${cityTime}</div>
+      </div>
+      <p class="back"> ← Back to all cities</p>
+    `;
+  }
+
+  update();
+  clearInterval(interval);
+  interval = setInterval(update, 1000);
+}
+
+function updateCity(event) {
+  let cityTz = event.target.value;
+
+  if (!cityTz) return;
+
+  if (cityTz === "current") {
+    cityTz = moment.tz.guess();
+  }
+
+  showSingleCity(cityTz);
 }
 
 updateCityTime();
-setInterval(updateCityTime, 1000);
+interval = setInterval(updateCityTime, 1000);
 
 let selectCity = document.querySelector("#select");
 selectCity.addEventListener("change", updateCity);
+
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("back")) {
+    clearInterval(interval);
+    updateCityTime();
+    interval = setInterval(updateCityTime, 1000);
+  }
+});
