114a0643c6de6f5d810cff34dbd8200c3713b85e
Pezza242 <periyearwood@gmail.com>
2026-05-05 17:11:42 +0100

    Added HTML to time format in JS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ let tzDate = moment.tz("Europe/Paris").format("Do MMMM YYYY");
 date.innerHTML = tzDate;
 
 let time = document.querySelector("#city-time");
-let tzTime = moment.tz("Europe/Paris").format("LTS");
+let tzTime = moment.tz("Europe/Paris").format("H:mm:ss [<small>]A[</small>]");
 
 time.innerHTML = tzTime;
